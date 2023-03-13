39e5d314a184e0682a00baa9c2ce291b4c871b72
Kauan <kauan.s.leite@hotmail.com>
2023-03-13 15:37:01 -0300

    3° Requisito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -23,13 +23,15 @@ def fetch(url):
 # Requisito 2
 def scrape_updates(html_content):
     selector = Selector(text=html_content)
-    urls_noticias = selector.css('.cs-overlay-link::attr(href)').getall()
-    return urls_noticias
+    url_news = selector.css('.cs-overlay-link::attr(href)').getall()
+    return url_news
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page_link = selector.css('.next::attr(href)').get()
+    return next_page_link
 
 
 # Requisito 4
@@ -40,7 +42,3 @@ def scrape_news(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
-
-
-# html = fetch('https://blog.betrybe.com/')
-# scrape_updates(html)
